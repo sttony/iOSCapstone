dedd55625be704e2ba1aeb7ff8c978f0ae79fe2a
sttony <sttony1984@gmail.com>
2024-02-28 16:22:08 -0800

    save2

--- finalGrade/UserProfile.swift
+++ finalGrade/UserProfile.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct UserProfile: View {
+    @Environment(\.presentationMode) var presentation
+    
+    let firstName = UserDefaults.standard.string(forKey: kFirstName) ?? "";
+    let lastName = UserDefaults.standard.string(forKey: kLastName) ?? "" ;
+    let email = UserDefaults.standard.string(forKey: kEmail) ?? "";
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Personal information")
+            Image("profile-image-placeholder")
+            Text(firstName)
+            Text(lastName)
+            Text(email)
+            Button(action: {
+                
+            }, label: {
+                Text("Logout")
+            }).padding(.init(top: 10, leading: 30, bottom: 10, trailing: 30))
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(20)
+                .padding(.top, 10)
+        }
     }
 }
 
