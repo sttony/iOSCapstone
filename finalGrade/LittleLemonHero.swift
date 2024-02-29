8f42301293199a25b38d3addfe8e1a52898ac5aa
sttony <sttony1984@gmail.com>
2024-02-28 16:22:59 -0800

    save3

--- finalGrade/LittleLemonHero.swift
+++ finalGrade/LittleLemonHero.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct LittleLemonHero: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            Text("Little lemon")
+                .font(.title)
+                .foregroundColor(Color(red: 0xf4/255.0, green: 0xce/255.0, blue:0x14/255.0))
+            Text("Chicago")
+                .font(.title2)
+                .foregroundColor(Color.white)
+            HStack{
+                Text("""
+                We are a family owned
+                Mediterranean restaurant,
+                focused on traditional
+                recipes served with a
+                modern twist.
+                """)
+                .foregroundColor(Color.white)
+                Image("hero-logo")
+                    .resizable(resizingMode: .stretch)
+                    .frame(width: 120.0, height: 120.0)
+                    
+            }
+            
+        }
+        .padding(5)
+        .background(Color(red: 0x49/255.0, green: 0x5e/255.0, blue: 0x57/255.0))
     }
 }
 
--- finalGrade/Menu.swift
+++ finalGrade/Menu.swift
@@ -6,10 +6,121 @@
 //
 
 import SwiftUI
+import CoreData
+import Foundation
+
 
 struct Menu: View {
+    @State var searchText = ""
+    @Environment(\.managedObjectContext) private var context
+    
+    
+    
+    static func createDishesFrom(menuItems: [MenuItem], _ context: NSManagedObjectContext) {
+        for menuItem in menuItems {
+            if exists(name: menuItem.title, context:context) != nil {
+                continue
+            }
+            let oneDish = Dish(context: context)
+            oneDish.title = menuItem.title
+            if let price = Float(menuItem.price) {
+                oneDish.price = price
+            }
+            oneDish.dishDescription = menuItem.description
+            oneDish.image = menuItem.image
+        }
+        try? context.save()
+    }
+    static func exists(name: String, context: NSManagedObjectContext) -> Dish? {
+        let fetchRequest: NSFetchRequest<Dish> = Dish.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "title == %@", name)
+        fetchRequest.fetchLimit = 1
+        
+        do {
+            let result = try context.fetch(fetchRequest)
+            return result.first
+        } catch {
+            print("Error fetching entity: \(error)")
+            return nil
+        }
+    }
+    
+    private func getMenuData(){
+        var menuItems: [MenuItem] = []
+        let url = URL(string: "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            // Handle the response
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("Invalid response")
+                return
+            }
+            
+            if !(200..<300).contains(httpResponse.statusCode) {
+                print("Unexpected status code: \(httpResponse.statusCode)")
+                return
+            }
+            
+            if let data = data {
+                do {
+                    let fullMenu = try JSONDecoder().decode(JSONMenu.self, from: data)
+                    menuItems = fullMenu.menu
+                    Menu.createDishesFrom(menuItems: menuItems, context)
+                }catch{
+                    NSLog("Decode failed %s", String(data: data, encoding: .utf8)!)
+                }
+            }
+            else{
+                return
+            }
+        }
+        task.resume()
+    }
+    
+    private func buildPredicate()-> NSPredicate{
+        if searchText.isEmpty {
+            return NSPredicate(value: true)
+        }
+        else{
+            return NSPredicate(format: "title CONTAINS[c] %@", searchText)
+        }
+    }
+    
+    private func buildSortDescriptors()-> [NSSortDescriptor]{
+        return [NSSortDescriptor(key: "title", ascending: true, selector: #selector(NSString.localizedCompare))]
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            LittleLemonLog()
+            LittleLemonHero()
+            NavigationView {
+                FetchedObjects(
+                    predicate:buildPredicate(),
+                    sortDescriptors: buildSortDescriptors()
+                ) {
+                    (dishes: [Dish]) in
+                    List {
+                        ForEach(dishes, id:\.self) { dish in
+                            DisplayDish(dish)
+                            //                                    .onTapGesture {
+                            //                                        showAlert.toggle()
+                            //                                    }
+                        }
+                    }
+                    .searchable(text: $searchText,
+                                prompt: "search...")
+                }
+            }
+        }
+        .onAppear(){
+            self.getMenuData()
+        }
     }
 }
 
